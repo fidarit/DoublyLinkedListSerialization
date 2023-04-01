5c04952f13c2c96e2154280f4cd7b5db85e6d40c
Fidarit Mullayanov <fidarit.mullayanov@yandex.ru>
2023-04-02 00:34:03 +0500

    Добавьте файлы проекта.

---
+++ Code.cs
@@ -0,0 +1,113 @@
+﻿using System.Text;
+
+class ListNode
+{
+    public ListNode Prev;
+    public ListNode Next;
+    public ListNode Rand; // произвольный элемент внутри списка
+    public string Data;
+}
+
+
+class ListRand
+{
+    public ListNode Head;
+    public ListNode Tail;
+    public int Count;
+
+    public void Serialize(FileStream s)
+    {
+        int[] randIndexes = new int[Count];
+        ListNode current = Head;
+        ListNode current2 = current.Next;  
+        for (int i = 0, j = 0; i < Count && j < Count; i++)
+        {
+            if (current != current2 && current.Rand == current2)
+            {
+                randIndexes[j] = i;
+                current = current.Next;
+                current2 = Head;
+                j++;
+                i = 0;
+            }
+            current2 = current2.Next;
+        }
+
+        //запись в файл
+        s.Write(BitConverter.GetBytes(Count));
+        current = Head;
+        for (int i = 0; i < Count; i++)
+        {
+            s.Write(BitConverter.GetBytes(randIndexes[i]));
+            s.Write(BitConverter.GetBytes(current.Data.Length));
+            s.Write(Encoding.UTF8.GetBytes(current.Data));
+            current = current.Next;
+        }
+    }
+
+    public void Deserialize(FileStream s)
+    {
+        byte[] bytes = new byte[4];
+        s.Read(bytes);
+        Count = BitConverter.ToInt32(bytes);
+
+        Head = new ListNode();
+        ListNode current = Head;
+        int[] randIndexes = new int[Count];
+
+        for (int i = 0; i < Count; i++)
+        {
+            if (i == 0)
+            {
+                bytes = new byte[4];
+                s.Read(bytes);
+                randIndexes[i] = BitConverter.ToInt32(bytes);
+
+                s.Read(bytes);
+                int dataSize = BitConverter.ToInt32(bytes);
+
+                bytes = new byte[dataSize];
+                s.Read(bytes);
+                current.Data = Encoding.UTF8.GetString(bytes);
+
+                current.Next = new ListNode();
+            }
+            else
+            {
+                current.Next.Prev = current;
+                current = current.Next;
+
+                bytes = new byte[4];
+                s.Read(bytes);
+                randIndexes[i] = BitConverter.ToInt32(bytes);
+
+                s.Read(bytes);
+                int dataSize = BitConverter.ToInt32(bytes);
+
+                bytes = new byte[dataSize];
+                s.Read(bytes);
+                current.Data = Encoding.UTF8.GetString(bytes);
+
+                if (i != Count - 1)
+                    current.Next = new ListNode();
+                else
+                    Tail = current;
+            }
+        }
+
+        current = Head;                 
+        ListNode current2 = current;      
+        for (int i = 0, j = 0; i < Count; i++)
+        {
+            if (randIndexes[j] == i)
+            {
+                current.Rand = current2;
+                current = current.Next;
+                j++;
+                current2 = Head;
+                i = 0;
+            }
+            current2 = current2.Next;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] data = new[]
+        {
+            "a",
+            "bb",
+            "ccc",
+            "dddd",
+            "eee",
+            "ff",
+            "g",
+        };
+
+        ListRand listRand = StringArrayToListRand(data);
+
+        using (FileStream stream = new FileStream("result.bin", FileMode.OpenOrCreate))
+        {
+            listRand.Serialize(stream);
+            
+            stream.Seek(0, SeekOrigin.Begin);
+            listRand = new ListRand();
+            listRand.Deserialize(stream);
+        }
+
+        string[] savedData = ListRandToStringArray(listRand);
+        foreach (string s in savedData)
+            Console.WriteLine(s);
+
+        Console.ReadLine();
+    }
+
+    public static ListRand StringArrayToListRand(string[] data)
+    {
+        ListRand listRand = new ListRand();
+        Random rand = new Random();
+
+        ListNode current = new ListNode();
+        listRand.Head = current;
+        listRand.Count = data.Length;
+        ListNode[] nodes = new ListNode[data.Length];
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            current.Data = data[i];
+
+            if (i != data.Length - 1)
+            {
+                current.Next = new ListNode();
+                current.Next.Prev = current;
+            }
+            else
+                listRand.Tail = current;
+
+            nodes[i] = current;
+            current = current.Next;
+        }
+
+        if(nodes.Length > 1)
+        {
+            int lastIndex = nodes.Length - 1;
+            for (int i = 0; i < nodes.Length; i++)
+                nodes[i].Rand = nodes[lastIndex - i];
+
+            if(nodes.Length % 2 == 1)
+            {
+                int midIndex = nodes.Length / 2;
+                nodes[midIndex].Rand = nodes[0].Rand;
+                nodes[0].Rand = nodes[midIndex];
+            }
+        }
+
+        return listRand;
+    }
+
+    public static string[] ListRandToStringArray(ListRand listRand)
+    {
+        string[] result = new string[listRand.Count];
+        ListNode current = listRand.Head;
+        for (int i = 0; i < listRand.Count; i++)
+        {
+            result[i] = current.Data;
+            current = current.Next;
+        }
+
+        return result;
+    }
+}
